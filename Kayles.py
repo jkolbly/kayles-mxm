4e286c228a390e9c5f02bf804f75482bda2dde78
Jason Kolbly <jason@rscheme.org>
2024-10-02 21:23:26 -0500

    Implement simple_grundy with no optimizations

--- Kayles.py
+++ Kayles.py
@@ -1,7 +1,30 @@
+import itertools
+
 # Calculate the Grundy number for the state n.
 # That is, the state with a single line of n adjacent stones.
 def simple_grundy(n):
-  pass
+  if n == 0:
+    return 0
+
+  # The grundy numbers  of accessible states
+  # Uses a set for O(1) lookup when finding the mex
+  grundys = set()
+
+  # Check states that are removals of 1
+  for i in range((n - 1) // 2 + 1):
+    grundys.add(grundy(i, n - i - 1))
+
+  # Check states that are removals of 2
+  for i in range((n - 2) // 2 + 1):
+    grundys.add(grundy(i, n - i - 2))
+
+  return mex(grundys)
+
+# Find the minimum excluded natural number from a set
+def mex(s):
+  for i in itertools.count(start=0):
+    if i not in s:
+      return i
 
 # Calculate the Grundy number for a state.
 # The state is specified as a list of integers representing
